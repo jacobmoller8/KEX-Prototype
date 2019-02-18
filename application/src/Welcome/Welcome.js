f62c64585cc5c0efb1ee8cdc289f8d980fc6dabf
Jacob Moller <jacob.moller8@hotmail.com>
2019-02-18 13:59:28 +0100

    work on loginpage layout

--- application/src/Welcome/Welcome.js
+++ application/src/Welcome/Welcome.js
@@ -6,8 +6,37 @@ class Welcome extends Component {
     render() {
         return (
             <div className="Welcome">
+                <div className="container-fluid" id="header">
+                    <h1>Welcome to our amazing app</h1>
+                </div>
 
-                <h1>Welcome to our amazing app</h1>
+                <div className="row justify-content-center" >
+                    <div className="col-4 text-center">
+
+                        <div id="signInContainer">
+                            <form>
+                                <div className="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="InputPassword" placeholder="Password"></input>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Sign In</button>
+                            </form>
+                        </div>
+
+                        <p id="registerText">Haven't got an account?</p>
+                        <button type="submit" className="btn btn-primary">Register</button>
+
+                    </div>
+
+
+                </div>
+
+
+
 
             </div>
         );
