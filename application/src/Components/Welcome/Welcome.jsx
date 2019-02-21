d9c6a0d6e1224995333f07792c8815d2f6f8ca33
Jacob Moller <jacob.moller8@hotmail.com>
2019-02-21 17:18:39 +0100

    import redux on welcome

--- application/src/Components/Welcome/Welcome.jsx
+++ application/src/Components/Welcome/Welcome.jsx
@@ -3,7 +3,43 @@ import './Welcome.css';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 
+import { connect } from "react-redux";
+import store from "../../Store/store";
+import { addUser, fetchUser } from "../../Actions/firebaseActions";
+
+
 class Welcome extends Component {
+
+    _isMounted = false;
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: store.getState().user
+        }
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+
+        store.subscribe(() => {
+            if (this._isMounted) {
+                this.setState({
+                    user: store.getState().user
+                });
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    onUpdateUser() {
+        this.props.addUser("ReactTest2");
+        this.props.fetchUser("Red");
+    }
+
     render() {
         return (
             <div className="Welcome">
@@ -42,4 +78,13 @@ class Welcome extends Component {
     }
 }
 
-export default Welcome;
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+const mapActionsToProps = {
+    addUser: addUser,
+    fetchUser: fetchUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(Welcome);
