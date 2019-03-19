6347dd607e8a985dd7a97356cf387a510a2105fc
Hugo B <hugo@bergqvist.com>
2019-03-19 21:11:12 +0100

    fixed margin in editItem form

--- application/src/index.js
+++ application/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 import { Provider } from 'react-redux';
