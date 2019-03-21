269c111a1dfbefb837a078456e1c4d012d2e969b
Hugo B <hugo@bergqvist.com>
2019-03-21 22:20:28 +0100

    fixed logo bug

--- application/src/Components/Presentation/Header/Header.jsx
+++ application/src/Components/Presentation/Header/Header.jsx
@@ -3,22 +3,27 @@ import './Header.css';
 import { Button } from 'react-bootstrap'
 
 
-let btnDisplay = {display: 'none'}
-let logoDisplay = {display: 'block'}
 export default class Header extends Component {
 
 	render() {
+
+		let btnDisplay = {display: 'none'}
+		let logoDisplay = {display: 'block'}
+		let profileLogo = {display: 'none'}
+
 		if (this.props.isLoggedIn) {
 			btnDisplay = { display: 'block' };
 			logoDisplay = { display: 'none'};
+			profileLogo = {display: 'inline-block'}
 		} else {
 			btnDisplay = { display: 'none' };
 			logoDisplay = { display: 'block'};
+			profileLogo = {display: 'none'}
 		}
 
 
-		let profileIcon = null
-		let logo = <div className="container-fluid col-8 offset-2 offset-md-4 col-md-4 logoContainer" style={logoDisplay}> <img className="logo" src={require('../../../Images/logo.svg')} alt="logo"></img></div>
+		let profileIcon = <Button className="profileBtn" style={profileLogo}>  <img className="profileImage" src={require('../../../Images/ProfileImages/carrot5.png')} alt="shoppingIcon"></img> </Button>
+		let logo = <div className="container-fluid col-8 offset-2 offset-md-4 col-md-4 logoContainer" style={logoDisplay}> <img className="logo" src={require('../../../Images/logo.svg')} alt="logo"></img></div>;
 		if (this.props.currentUser === "Red"){
 			profileIcon = <Button className="profileBtn">  <img className="profileImage" src={require('../../../Images/ProfileImages/RedProfile.svg')} alt="shoppingIcon"></img> </Button>
 		}else if(this.props.currentUser === "Blue"){
