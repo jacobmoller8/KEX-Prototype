6b99d9067aad3d83dc4a252c820a0aec5cf1edb1
Hugo B <hugo@bergqvist.com>
2019-03-21 22:01:09 +0100

    added logo to LogIn Page

--- application/src/Components/Presentation/Header/Header.jsx
+++ application/src/Components/Presentation/Header/Header.jsx
@@ -2,30 +2,23 @@ import React, { Component } from 'react'
 import './Header.css';
 import { Button } from 'react-bootstrap'
 
+
+let btnDisplay = {display: 'none'}
+let logoDisplay = {display: 'block'}
 export default class Header extends Component {
-	constructor(props) {
-		super(props)
-
-		this.ifLoggedIn = this.ifLoggedIn.bind(this)
-	}
-
-
-	ifLoggedIn() {
-		const { isLoggedIn } = this.props
-		var btnDisplay = '';
-
-		if (isLoggedIn) {
-			btnDisplay = { display: 'block' };
-		} else {
-			btnDisplay = { display: 'none' };
-		}
-
-		return btnDisplay
-
-	}
 
 	render() {
-		let profileIcon = <Button className="profileBtn">  <img className="profileImage" src={require('../../../Images/ProfileImages/carrot5.png')} alt="shoppingIcon"></img> </Button>
+		if (this.props.isLoggedIn) {
+			btnDisplay = { display: 'block' };
+			logoDisplay = { display: 'none'};
+		} else {
+			btnDisplay = { display: 'none' };
+			logoDisplay = { display: 'block'};
+		}
+
+
+		let profileIcon = null
+		let logo = <div className="container-fluid col-8 offset-2 offset-md-4 col-md-4 logoContainer" style={logoDisplay}> <img className="logo" src={require('../../../Images/logo.svg')} alt="logo"></img></div>
 		if (this.props.currentUser === "Red"){
 			profileIcon = <Button className="profileBtn">  <img className="profileImage" src={require('../../../Images/ProfileImages/RedProfile.svg')} alt="shoppingIcon"></img> </Button>
 		}else if(this.props.currentUser === "Blue"){
@@ -43,9 +36,10 @@ export default class Header extends Component {
 		return (
 			<div className="row">
 				<div className="container-fluid header">
-					<Button variant="warning" className="signOutBtn" onClick={this.props.onLogoutClick} style={this.ifLoggedIn()}>Log out</Button>
+					<Button variant="warning" className="signOutBtn" onClick={this.props.onLogoutClick} style={btnDisplay}>Log out</Button>
 				</div>
 				<div className="arch" />
+					{logo}
 				<div className="container-fluid col-8 offset-2 offset-sm-4 col-sm-4 profileBtnContainer">
 					{profileIcon}
 				</div>
